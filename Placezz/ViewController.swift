6215c7d91ae02d7985aee5a0a01ccfb37048fb22
russell-archer <russell.archer@mac.com>
2020-05-26 17:40:39 +0100

    Initial Commit

---
+++ Placezz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Placezz
+//
+//  Created by Russell Archer on 26/05/2020.
+//  Copyright © 2020 Russell Archer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
